4bf5e032abd8e99b1247d89859b5230785617119
wmr <wmr101@gmail.com>
2014-06-09 23:41:29 +0200

    [++] extended set of zsh aliases

--- home/.custom/aliases.zsh
+++ home/.custom/aliases.zsh
@@ -1,4 +1,100 @@
-# custom zsh aliases
+# zsh aliases
+
 alias plz=sudo
 alias zshconf="${EDITOR} ~/.zshrc"
 alias ltrah='ls -ltrah'
+
+alias ack='nocorrect ack'
+alias cd='nocorrect cd'
+alias cp='nocorrect cp'
+alias ebuild='nocorrect ebuild'
+alias gcc='nocorrect gcc'
+alias gist='nocorrect gist'
+alias grep='nocorrect grep'
+alias heroku='nocorrect heroku'
+alias ln='nocorrect ln'
+alias man='nocorrect man'
+alias mkdir='nocorrect mkdir'
+alias mv='nocorrect mv'
+alias mysql='nocorrect mysql'
+alias rm='nocorrect rm'
+
+# Disable globbing.
+alias bower='noglob bower'
+alias fc='noglob fc'
+alias find='noglob find'
+alias ftp='noglob ftp'
+alias history='noglob history'
+alias locate='noglob locate'
+alias rake='noglob rake'
+alias rsync='noglob rsync'
+alias scp='noglob scp'
+alias sftp='noglob sftp'
+
+# Define general aliases.
+alias _='sudo'
+alias b='${(z)BROWSER}'
+alias e='${(z)VISUAL:-${(z)EDITOR}}'
+alias p='${(z)PAGER}'
+alias po='popd'
+alias pu='pushd'
+alias type='type -a'
+
+alias ls='ls -G'
+alias l='ls -1A'         # Lists in one column, hidden files.
+alias ll='ls -lh'        # Lists human readable sizes.
+alias lr='ll -R'         # Lists human readable sizes, recursively.
+alias la='ll -A'         # Lists human readable sizes, hidden files.
+alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+alias llk='ll -Sr'        # Lists sorted by size, largest last.
+alias llt='ll -tr'        # Lists sorted by date, most recent last.
+alias llc='llt -c'         # Lists sorted by date, most recent last, shows change time.
+alias llu='llt -u'         # Lists sorted by date, most recent last, shows access time.
+if (( $+commands[curl] )); then
+  alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
+fi
+
+if (( $+commands[htop] )); then
+  alias top=htop
+else
+  alias topc='top -o cpu'
+  alias topm='top -o vsize'
+fi
+
+# Makes a directory and changes to it.
+function mkdcd {
+  [[ -n "$1" ]] && mkdir -p "$1" && builtin cd "$1"
+}
+
+# Changes to a directory and lists its contents.
+function cdls {
+  builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pushes an entry onto the directory stack and lists its contents.
+function pushdls {
+  builtin pushd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pops an entry off the directory stack and lists its contents.
+function popdls {
+  builtin popd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Prints columns 1 2 3 ... n.
+function slit {
+  awk "{ print ${(j:,:):-\$${^@}} }"
+}
+
+# Finds files and executes a command on them.
+function find-exec {
+  find . -type f -iname "*${1:-}*" -exec "${2:-file}" '{}' \;
+}
+
+
+
+
+# Serves a directory via HTTP.
+alias http-serve='python -m SimpleHTTPServer'
+
+
